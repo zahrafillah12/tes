b71f331197e31edfe33e8220bcbfc864b0c1d321
zahrafillah12 <zahra_shafir_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:19:57 +0700

    tes

---
+++ .java
@@ -0,0 +1,5 @@
+public class tes {}
+public static void main(string[]args) {
+    string nama="zahra"
+    string kelas="x rpl 5"
+    system.out.print("halo,nama saya zahra fillah lita shafir" "dari kelas 10 rpl 5"
