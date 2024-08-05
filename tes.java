0a52156cb449d97ee648bd9839dc05efb52daf81
zahrafillah12 <zahra_shafir_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 14:28:15 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,7 @@
+public class tes {
+    public static void main(String[] args) {
+        String nama = "zahra";
+        String kelas = "x rpl 5";
+        System.out.print("halo,nama saya" + nama + "dari kelas" + kelas);
+    }
+}
